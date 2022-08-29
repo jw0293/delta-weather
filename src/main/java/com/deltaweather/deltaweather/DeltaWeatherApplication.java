414ab7712572fc9686eba46032163776c4d96bb6
moonn6pence <shin421179@gmail.com>
2022-08-29 19:30:55 +0900

    init

---
+++ src/main/java/com/deltaweather/deltaweather/DeltaWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.deltaweather.deltaweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeltaWeatherApplication.class, args);
+	}
+
+}
